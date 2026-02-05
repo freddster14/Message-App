4722c1886aec5c60f57d3cce65ea54d053d8df02
freddster14 <freddycarrillo951@gmail.com>
2026-02-04 20:22:07 -0600

    Update SOCKET_URL

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = import.meta.env.MODE === 'production' ? '/api' : 'http://localhost:3000';
+const SOCKET_URL = import.meta.env.MODE === 'production' ? 'https://message-app-gvu9.onrender.com' : 'http://localhost:3000';
 
 export const socket = io(SOCKET_URL, {
   autoConnect: false,

da33eb3f61a145db84222090e3830e9bc78cdd6b
freddster14 <freddycarrillo951@gmail.com>
2026-02-11 16:14:21 -0600

    Add ApiError class to handle errors

--- client/src/api/client.js
+++ client/src/api/client.js
@@ -1,25 +1,22 @@
 const API_URL = import.meta.env.MODE === 'production' ? '/api' : 'http://localhost:3000';
+class ApiError extends Error {
+  constructor(message, status) {
+    super(message);
+    this.status = status;
+    this.name = "ApiError"
+  }
+}
 
 export default async function apiFetch(url, options) {
-  try {
-    const res = await fetch(`${API_URL}${url}`, {
-      ...options,
-      headers: {
-        "Content-Type": "application/json",
-      },
-      credentials: "include",
-    });
-    
-    const data = await res.json();
-
-    if (!res.ok) {
-      throw new Error(data.msg || 'Server');
-    }
-
-    return data;
-  } catch (error) {
-    
-    console.error(error);
-    throw error;
-  }
+  const res = await fetch(`${API_URL}${url}`, {
+    ...options,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+  });
+  
+  const info = await res.json();
+  if(!res.ok) throw new ApiError(`${info.msg || 'Something went wrong'}`, { code: res.status, text: res.statusText });
+  return info;  
 }
